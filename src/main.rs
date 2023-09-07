1651b4fba3265fd088e361fe33f71ba16421d9ad
Arian Shahmirza <arishah@users.noreply.github.com>
2023-09-06 22:52:22 -0400

    add fft

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
 use std::cmp::min;
 use std::collections::LinkedList;
 use std::env;
+use num_complex::Complex;
+use std::f64::consts::PI;
+use rand::Rng;
 
 const N: usize = 128;
 const K: usize = 3;
@@ -9,6 +12,25 @@ const K: usize = 3;
 static mut GLOBAL_DMC: f64 = 0.0;
 
 unsafe fn update_dmc(sampler: &mut LinkedList<u64>, addr_ptr: *const f32) {
+    let addr = addr_ptr as u64;
+
+    let mut pos: usize = 0;
+    let mut it_prev = sampler.iter_mut().peekable();
+    while let Some(val) = it_prev.next() {
+        if *val == addr {
+            it_prev.next();
+            GLOBAL_DMC += (pos as f64).sqrt();
+            break;
+        }
+        pos += 1;
+    }
+    if pos != sampler.len() {
+        sampler.remove(pos as usize);
+    }
+    sampler.push_front(addr);
+}
+
+unsafe fn update_dmc_complex(sampler: &mut LinkedList<u64>, addr_ptr: *const Complex<f64>) {
     let addr = addr_ptr as u64;
 
     let mut pos: usize = 0;
@@ -55,12 +77,63 @@ fn conv2d(image_i: &mut [f32], image_k: &mut [f32], result: &mut [f32], c: usize
     }
 }
 
+fn fft(f: &mut Vec<Complex<f64>>, ln: usize, invert: bool) {
+    let n = 1 << ln;
+    let half_n = n / 2;
+    let mut j = 1;
+    let mut sampler: LinkedList<u64> = LinkedList::new();
+
+    for i in 1..n {
+        if i < j {
+            f.swap(i - 1, j - 1);
+        }
+        let mut k = half_n;
+        while k < j {
+            j -= k;
+            k /= 2;
+        }
+        j += k;
+    }
+
+    for l in 1..=ln {
+        let k = 1 << l;
+        let half_k = k / 2;
+        let mut u = Complex::new(1.0, 0.0);
+        let mut w = Complex::from_polar(1.0, -PI / half_k as f64);
+
+        if invert {
+            w = w.conj();
+        }
+
+        for j in 1..=half_k {
+            for i in (j..=n).step_by(k) {
+                let m = i + half_k;
+                let t = f[m - 1] * u;
+                f[m - 1] = f[i - 1] - t;
+                f[i - 1] = f[i - 1] + t;
+                unsafe {
+                    update_dmc_complex(&mut sampler, &f[m - 1]);
+                    update_dmc_complex(&mut sampler, &f[i - 1]);
+                }
+            }
+            u *= w;
+        }
+    }
+
+    if invert {
+        for elem in f.iter_mut() {
+            *elem /= n as f64;
+        }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    assert_eq!(args.len(), 3);
+    let mode = args[1].parse::<String>().unwrap();
 
-    let channels = args[1].parse::<usize>().unwrap();
-    let batch_sz: i32 = args[2].parse::<i32>().unwrap();
+    if mode == "conv" {
+    let channels = args[2].parse::<usize>().unwrap();
+    let batch_sz: i32 = args[3].parse::<i32>().unwrap();
     let batch_sz = if batch_sz == -1 {
         channels as i32
     } else {
@@ -72,5 +145,30 @@ fn main() {
     let mut image_k = vec![0.0; K * K];
     conv2d(&mut image_i, &mut image_k, &mut result, channels, batch_sz as usize);
     println!("DMC: {}", unsafe { GLOBAL_DMC });
+    } else {
+    let vec_size = args[2].parse::<usize>().unwrap();
+    let len = args[3].parse::<usize>().unwrap();
+    let mut rng = rand::thread_rng();
+    let mut f: Vec<Complex<f64>> = (0..vec_size)
+    .map(|_| {
+        let real_part = rng.gen_range(-1.0..1.0); // Adjust the range as needed
+        let imag_part = rng.gen_range(-1.0..1.0); // Adjust the range as needed
+        Complex::new(real_part, imag_part)
+    })
+    .collect();
+
+    for complex in &f {
+        println!("before: {}", complex);
+    }
+
+    fft(&mut f, len, false);
+
+    println!("DMC: {}", unsafe { GLOBAL_DMC });
+
+/*     for complex in &f {
+        println!("after: {}", complex);
+    }*/
+}
+
     return ();
 }
