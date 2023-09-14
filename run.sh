39e39934e943a364e536e0c44e90b9d6b820686d
arishah <arishah@users.noreply.github.com>
2023-09-13 22:16:07 -0400

    add fft run

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@ min=$1
 max=$2
 for i in `seq $min $max`
 do
-    echo c = "$i" >> /home/ec2-user/results/one/out_${max}
-    /home/ec2-user/convolution/target/release/convolution $i 1 >> /home/ec2-user/results/one/out_${max}
+    echo c = "$i" >> /home/ec2-user/results/fft_2/out_${max}
+#    /home/ec2-user/convolution/target/release/convolution conv $i 1 >> /home/ec2-user/results/one/out_${max}
+#    /home/ec2-user/convolution/target/release/convolution fft 1048577 $i  >> /home/ec2-user/results/fft/out_${max}
+    first=`echo 2^$i |bc`
+    echo $first
+    /home/ec2-user/convolution/target/release/convolution fft $first $i  >> /home/ec2-user/results/fft_2/out_${max}
 done
 
