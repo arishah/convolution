ba9bc413745ebab62474c1201fcadf388c95e92b
arishah <arishah@users.noreply.github.com>
2023-09-21 15:53:29 +0000

    remove extra message

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ do
 #    /home/ec2-user/convolution/target/release/convolution conv $i 1 >> /home/ec2-user/results/one/out_${max}
 #    /home/ec2-user/convolution/target/release/convolution fft 1048577 $i  >> /home/ec2-user/results/fft/out_${max}
     first=`echo 2^$i |bc`
-    echo $first
     /home/ec2-user/convolution/target/release/convolution fft $first $i  >> /home/ec2-user/results/fft_2/out_${max}
 done
 
